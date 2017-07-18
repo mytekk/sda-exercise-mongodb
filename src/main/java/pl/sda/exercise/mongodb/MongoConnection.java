a5ad898016ede040dfb0e3626800306f06638398
Bartek Mytko <bartlomiej.mytko@gmail.com>
2017-07-18 18:52:46 +0200

    Listing all documents from collection Book (ex. 2)

--- src/main/java/pl/sda/exercise/mongodb/MongoConnection.java
+++ src/main/java/pl/sda/exercise/mongodb/MongoConnection.java
@@ -4,5 +4,5 @@ package pl.sda.exercise.mongodb;
  * Created by RENT on 2017-07-18.
  */
 public class MongoConnection {
-    public static final String CONNECTIONSTRING = "mongodb://xxx:xxx@dxxx/books";
+    public static final String CONNECTIONSTRING = "mongodb://xxx:xxx@xxx/books";
 }
--- src/main/java/pl/sda/exercise/mongodb/MongoDB02.java
+++ src/main/java/pl/sda/exercise/mongodb/MongoDB02.java
@@ -1,12 +1,39 @@
 package pl.sda.exercise.mongodb;
 
+import com.mongodb.Block;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.ListCollectionsIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 /**
  * Exercise 02: get all books
  */
-public class MongoDB02 {
+public class MongoDB02 extends MongoConnection {
 
 	public static void main(String[] args) {
-		//TODO
+
+		//podlaczamy sie do serwera
+		MongoClient mongoClient = new MongoClient(new MongoClientURI(CONNECTIONSTRING));
+
+		//podlaczamy sie do bazy danych
+		MongoDatabase database = mongoClient.getDatabase("books");
+
+		//podłączamy sie do kolekcji books
+		MongoCollection<Document> collection = database.getCollection("books");
+
+		//wyszukanie wszystkich dokumentow z kolekcji wypisanie ich
+		collection.find().forEach(new Block<Document>() {
+			@Override
+			public void apply(Document document) {
+				System.out.println(document.toJson());
+			}
+		});
+
+
+		mongoClient.close();
 	}
 
 }
