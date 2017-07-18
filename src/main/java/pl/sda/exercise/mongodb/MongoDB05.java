e41ffddd1429e2547ae9ddd6114df7aa0a1eab9e
Bartek Mytko <bartlomiej.mytko@gmail.com>
2017-07-18 19:53:14 +0200

    Books sorted by title (ex. 5)

--- src/main/java/pl/sda/exercise/mongodb/MongoDB05.java
+++ src/main/java/pl/sda/exercise/mongodb/MongoDB05.java
@@ -1,12 +1,32 @@
 package pl.sda.exercise.mongodb;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.Block;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Sorts;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
 /**
  * Exercise 05: get books sorted by title
  */
-public class MongoDB05 {
+public class MongoDB05 extends MongoConnection {
 
 	public static void main(String[] args) {
-		//TODO
+
+		MongoClient mongoClient = new MongoClient(new MongoClientURI(CONNECTIONSTRING));
+		MongoDatabase database = mongoClient.getDatabase("books");
+		MongoCollection<Document> collection = database.getCollection("books");
+
+		//wyszukujemy o srtujemy
+		collection.find().sort(Sorts.ascending("title"))
+				.forEach((Block<Document>) document -> System.out.println(document.toJson()));
+
+
+		mongoClient.close();
 	}
 
 }
