911c1deadb61376e9eaaa768221fc2ecab333a0a
Bartek Mytko <bartlomiej.mytko@gmail.com>
2017-07-18 20:02:49 +0200

    Get books with only title (ex. 6)

--- src/main/java/pl/sda/exercise/mongodb/MongoDB06.java
+++ src/main/java/pl/sda/exercise/mongodb/MongoDB06.java
@@ -1,12 +1,34 @@
 package pl.sda.exercise.mongodb;
 
+import com.mongodb.Block;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Projections;
+import com.mongodb.client.model.Sorts;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
 /**
  * Exercise 06: get books with only title
  */
-public class MongoDB06 {
+public class MongoDB06 extends MongoConnection {
 
 	public static void main(String[] args) {
-		//TODO
+
+		MongoClient mongoClient = new MongoClient(new MongoClientURI(CONNECTIONSTRING));
+		MongoDatabase database = mongoClient.getDatabase("books");
+		MongoCollection<Document> collection = database.getCollection("books");
+
+		//wyszukujemy
+		Bson myInclude = Projections.include("title");
+		Bson myExclude = Projections.excludeId();
+		collection.find().projection(Projections.fields(myInclude,myExclude))
+				.forEach((Block<Document>) document -> System.out.println(document.toJson()));
+
+
+		mongoClient.close();
 	}
 
 }
