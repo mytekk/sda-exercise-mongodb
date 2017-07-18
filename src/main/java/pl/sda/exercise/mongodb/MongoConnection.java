393d7ba017a1747cfe73035fb20b2eb94012ef16
Bartek Mytko <bartlomiej.mytko@gmail.com>
2017-07-18 19:00:04 +0200

    Getting book by title (ex. 3)

--- src/main/java/pl/sda/exercise/mongodb/MongoConnection.java
+++ src/main/java/pl/sda/exercise/mongodb/MongoConnection.java
@@ -4,5 +4,6 @@ package pl.sda.exercise.mongodb;
  * Created by RENT on 2017-07-18.
  */
 public class MongoConnection {
+
     public static final String CONNECTIONSTRING = "mongodb://xxx:xxx@xxx/books";
 }
--- src/main/java/pl/sda/exercise/mongodb/MongoDB03.java
+++ src/main/java/pl/sda/exercise/mongodb/MongoDB03.java
@@ -1,12 +1,37 @@
 package pl.sda.exercise.mongodb;
 
+import com.mongodb.Block;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import org.bson.Document;
+
 /**
  * Exercise 03: get books by title
  */
-public class MongoDB03 {
+public class MongoDB03 extends MongoConnection {
 
 	public static void main(String[] args) {
-		//TODO
+
+		MongoClient mongoClient = new MongoClient(new MongoClientURI(CONNECTIONSTRING));
+		MongoDatabase database = mongoClient.getDatabase("books");
+		MongoCollection<Document> collection = database.getCollection("books");
+
+		//wyszukanie wszystkich dokumentow z kolekcji o zadanym tytule
+        //dokladnie rowny tytulowi
+		//collection.find(Filters.eq("title", "Pinokio")).forEach(new Block<Document>() {
+        //lub po wyrazeniu regularnym
+        collection.find(Filters.regex("title", "Pinok.*")).forEach(new Block<Document>() {
+            @Override
+            public void apply(Document document) {
+                System.out.println(document.toJson());
+            }
+        });
+
+
+		mongoClient.close();
 	}
 
 }
