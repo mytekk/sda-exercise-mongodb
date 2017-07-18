e0c7a7d18869780fa75d0a4596f341b533b7d76e
Bartek Mytko <bartlomiej.mytko@gmail.com>
2017-07-18 18:45:26 +0200

    Connecting to Mongo database, listing all collections (ex. 1)

---
+++ src/main/java/pl/sda/exercise/mongodb/MongoConnection.java
@@ -0,0 +1,8 @@
+package pl.sda.exercise.mongodb;
+
+/**
+ * Created by RENT on 2017-07-18.
+ */
+public class MongoConnection {
+    public static final String CONNECTIONSTRING = "mongodb://xxx:xxx@dxxx/books";
+}
--- src/main/java/pl/sda/exercise/mongodb/MongoDB01.java
+++ src/main/java/pl/sda/exercise/mongodb/MongoDB01.java
@@ -1,12 +1,40 @@
 package pl.sda.exercise.mongodb;
 
+import com.mongodb.Block;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.ListCollectionsIterable;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 /**
  * Exercise 01: get all collections
  */
-public class MongoDB01 {
+public class MongoDB01 extends MongoConnection {
 
 	public static void main(String[] args) {
 		//TODO
+
+		//podlaczamy sie do serwera
+		MongoClient mongoClient = new MongoClient(new MongoClientURI(CONNECTIONSTRING));
+
+		//podlaczamy sie do bazy danych
+		MongoDatabase database = mongoClient.getDatabase("books");
+
+		//pobieramy liste kolejkcji z bazy
+		ListCollectionsIterable<Document> listCollections = database.listCollections();
+
+		//listujemy kolekcje z bazy danych
+		//nowa instancja interfejsu block, pozwala na wykonywanie operacji na elementach MongoDB
+		listCollections.forEach(new Block<Document>() {
+			@Override
+			public void apply(Document document) {
+				//System.out.println(document.toJson());
+				System.out.println(document.get("name"));
+			}
+		});
+
+		mongoClient.close();
 	}
 
 }
