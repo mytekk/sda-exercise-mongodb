a246eecb0ed94c1fdbe8fa815fa9afccf4e09bdf
Bartek Mytko <bartlomiej.mytko@gmail.com>
2017-07-18 20:33:54 +0200

    Deleting collection (ex. 9)

--- src/main/java/pl/sda/exercise/mongodb/MongoDB09.java
+++ src/main/java/pl/sda/exercise/mongodb/MongoDB09.java
@@ -1,12 +1,33 @@
 package pl.sda.exercise.mongodb;
 
+import com.mongodb.Block;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
 /**
  * Exercise 09: delete collection people
  */
-public class MongoDB09 {
+public class MongoDB09 extends MongoConnection {
 
 	public static void main(String[] args) {
-		//TODO
+
+
+		MongoClient mongoClient = new MongoClient(new MongoClientURI(CONNECTIONSTRING));
+		MongoDatabase database = mongoClient.getDatabase("books");
+
+		System.out.println("Kolekcje przed usunieciem");
+		database.listCollections().forEach((Block<Document>) document -> System.out.println(document.toJson()));
+
+		//tworze i usuwam kolekcje
+		MongoCollection<Document> movies = database.getCollection("movies");
+		movies.drop();
+
+		System.out.println("Kolekcje po usunieciem");
+		database.listCollections().forEach((Block<Document>) document -> System.out.println(document.toJson()));
+
 	}
 
 }
